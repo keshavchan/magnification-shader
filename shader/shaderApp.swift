edc0fce87e1cf1b2afa8e178314ad93c0b77aa7d
keshavchan <keshavchauhan1729@gmail.com>
2025-10-02 10:36:33 +0530

    Initial Commit

---
+++ shader/shaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  shaderApp.swift
+//  shader
+//
+//  Created by keshav on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct shaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ shaderTests/shaderTests.swift
@@ -0,0 +1,17 @@
+//
+//  shaderTests.swift
+//  shaderTests
+//
+//  Created by keshav on 02/10/25.
+//
+
+import Testing
+@testable import shader
+
+struct shaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
